63bec4bebb75ee2161368bc30c1b57fe80774908
Sergio von Borries <sergiov@qubotech.com>
2018-04-11 19:15:52 -0400

    Refactor of Post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
   def index
   end
 
@@ -9,16 +10,21 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to @post
+      redirect_to @post, notice: "Post creado exitosamente"
+    else
+      redirect_to :new
     end
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   private
    def post_params
      params.require(:post).permit(:id, :date, :rationale)
+   end
+
+   def set_post
+     @post = Post.find(params[:id])
    end
 end
--- spec/features/post_spect.rb
+++ spec/features/post_spect.rb
@@ -14,14 +14,15 @@ describe 'navigate' do
   end
 
   describe 'creation' do
-    it 'has a new form to create Post' do
+    before do
       visit new_post_path
+    end
+    it 'has a new form to create Post' do
+
       expect(page.status_code).to eq(200)
     end
 
     it 'can be created from a form New Page' do
-      visit new_post_path
-
       fill_in 'post[date]', with: Time.zone.today
       fill_in 'post[rationale]', with: "Some rationale data"
 
