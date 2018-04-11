560592bad08755ceb0d77743cb3777a2b1c27006
Sergio von Borries <sergiov@qubotech.com>
2018-04-11 11:01:05 -0400

    Added User Validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    before do
+      @user = User.create(email: "test@test.com", password: "asdfasdf",
+        password_confirmation: "asdfasdf", first_name:"Juan", last_name: "Perez")
+    end
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+
+    it "cannot be created without first_name and last_name" do
+      @user.last_name = nil
+      @user.first_name = nil
+      expect(@user).to_not be_valid
+    end
+  end
 end
