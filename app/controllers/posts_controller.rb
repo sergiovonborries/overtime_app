ee0ba2386e8e460d08c5347c863a2917b3600279
Sergio von Borries <sergiov@qubotech.com>
2018-04-24 20:06:53 -0400

    Implemented edit functionality for post.status

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
 
   private
    def post_params
-     params.require(:post).permit(:id, :date, :rationale)
+     params.require(:post).permit(:id, :date, :rationale, :status)
    end
 
    def set_post
