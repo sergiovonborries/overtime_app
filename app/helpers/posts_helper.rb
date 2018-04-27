576cc53f3d5f4e90c6970b2f16f118f160b5fc39
Sergio von Borries <sergiov@qubotech.com>
2018-04-27 12:20:23 -0400

    Implemented status span

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+  def status_label status
+    case status
+    when 'submitted'
+      content_tag(:span, status.titleize, class: 'badge badge-primary')
+    when 'approved'
+      content_tag(:span, status.titleize, class: 'badge badge-success')
+    when 'rejected'
+      content_tag(:span, status.titleize, class: 'badge badge-danger')
+    end
+  end
 end
