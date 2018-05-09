b879a47a9b91a4bc7e362fd8670a8068930c155b
Sergio von Borries <sergiov@qubotech.com>
2018-05-09 10:22:01 -0400

    Updated Factory for audit_log and tested out 6 days default

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,10 @@ puts "1 Admin User created"
 100.times do |post|
   Post.create(date: Time.zone.today, rationale: "#{post} rationale", user_id: user.id, overtime_request: 2.5)
 end
-
 puts '100 Post have been created'
+
+100.times do |audit_log|
+  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts "100 audit logs have been created"
