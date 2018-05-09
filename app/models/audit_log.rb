9b43e90ffcc5cf6223b7c64246be66730ddc1a35
Sergio von Borries <sergiov@qubotech.com>
2018-05-09 11:00:51 -0400

    Integrated validations and defaults for audit log attributes

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,13 @@
 class AuditLog < ApplicationRecord
   belongs_to :user
+  validates_presence_of :status, :start_date, :user_id
+
+  after_initialize :set_defaults
+
+  private
+
+    def set_defaults
+      self.start_date ||= Date.today - 6.days
+    end
+
 end
